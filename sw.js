ab738eb490c7ff99d89d0fc4b1b2b50ea1b49c92
Dennis Kung <denniskungtw@gmail.com>
2026-03-24 23:10:31 +0800

    Deploy to GitHub Pages

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'gakujiigo-v1';
+
+// Cache essential assets on install
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll(['/', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'])
+    )
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first strategy: always try network, fall back to cache
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
